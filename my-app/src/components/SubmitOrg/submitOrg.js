6167de51aaee0ef38ebb5a0ef9f67048c4321888
ashlau <ashlau@uw.edu>
2020-11-22 18:13:40 -0800

    redid submit org form to send data to firebase, set browse page back to blank

--- my-app/src/components/SubmitOrg/submitOrg.js
+++ my-app/src/components/SubmitOrg/submitOrg.js
@@ -2,182 +2,165 @@ import React, { Component, useState } from 'react'
 import { db } from '../firebase'
 import './submitOrg.css'
 
-class submitOrg extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            Name: '',
-            Type: '',
-            Address: '',
-            Phone: '',
-            Email: '',
-            Need: '',
-            Description: '',
-            Mission: '',
-            info: []
-        }
 
-        this.handleNameChange = this.handleNameChange.bind(this)
-        this.handleTypeChange = this.handleTypeChange.bind(this)
-        this.handleAddressChange = this.handleAddressChange.bind(this)
-        this.handlePhoneChange = this.handlePhoneChange.bind(this)
-        this.handleEmailChange = this.handleEmailChange.bind(this)
-        this.handleNeedChange = this.handleNeedChange.bind(this)
-        this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
-        this.handleMissionChange = this.handleMissionChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+const Organization = () => {
+    const [name, setName] = useState("")
+    const [type, setType] = useState("")
+    const [address, setAddress] = useState("")
+    const [phone, setPhone] = useState("")
+    const [email, setEmail] = useState("")
+    const [need, setNeed] = useState("")
+    const [goal, setGoal] = useState("")
+    const [description, setDescription] = useState("")
 
-    handleNameChange(event) {
-        this.setState({Name: event.target.value})
-    }
-    handleTypeChange(event) {
-        this.setState({Type: event.target.value})
-    }
-    handleAddressChange(event) {
-        this.setState({Address: event.target.value})
-    }
-    handlePhoneChange(event) {
-        this.setState({Phone: event.target.value})
-    }
-    handleEmailChange(event) {
-        this.setState({Email: event.target.value})
-    }
-    handleNeedChange(event) {
-        this.setState({Need: event.target.value})
-    }
-    handleDescriptionChange(event) {
-        this.setState({Description: event.target.value})
-    }
+const handleSubmit = (e) => {
+    e.preventDefault()
 
-    handleMissionChange(event) {
-        this.setState({Mission: event.target.value})
-    }
+    db.collection('organizations').add({
+        name: name,
+        type: type,
+        address: address,
+        phone: phone,
+        email: email,
+        need: need,
+        goal: goal,
+        description: description,
+    })
+    .then(() => {
+        alert('Organization has been submitted')
+    })
+    .catch((error) => {
+        alert(error.message)
+    })
 
-    handleSubmit(event) {
-        var obj = {Name: this.state.Name,
-            Type: this.state.Type,
-            Address: this.state.Address,
-            Phone: this.state.Phone,
-            Email: this.state.Email,
-            Need: this.state.Need,
-            Description: this.state.Description,
-            Mission: this.state.Mission
-        }
-        
-        this.state.info.push(obj)
-        event.preventDefault()
-
-        this.clearState(event)
-        this.setState({})
-    }
-
-    clearState(event) {
-        /*
-        this.state.Name = ''
-        this.state.Type = ''
-        this.state.Address = ''
-        this.state.Phone = ''
-        this.state.Email = ''
-        this.state.Need = ''
-        this.state.Description = ''
-        this.state.Mission = ''
-        */
-
-        this.setState = ''
-    }
-
-    render() {
-        return (
-            <div>
-                <main id = "submit org">
-                    <div className="form-flex">
-                        <div id = "submit org form">
-                            <form>
-                                <h1>Submit a goal for your organization</h1>
-                                <div className="form-content">
-
-                                    <div className="organization-type">
-                                        <div className="form-row">
-                                            <label>
-                                                <div className ="label-text">What is your organization?</div>
-                                                <input type="text" id="name" placeholder="Organization name" value={this.state.Name} onChange={this.handleNameChange} />
-                                            </label>
-                                        </div>
-
-                                        <div className="form-row">
-                                            <label className="num">
-                                                <div className="label-text">Organization type:</div>
-                                                <select id="myList" value={this.state.Type} onChange={this.handleTypeChange}>
-                                                <option value="0">Select organization type</option>
-                                                <option value="1">Medical</option>
-                                                <option value="2">Educational</option>
-                                                <option value="3">Occupational</option>
-                                                <option value="4">Military</option>
-                                                </select>
-                                            </label>
-                                        </div>
-                                        
-                                    </div>
-
-                                    <div className="form-row">
-                                        <label>
-                                            <div className ="label-text">Where is your organization located?</div>
-                                            <input type="text" id="address" placeholder="Address" value={this.state.Address} onChange={this.handleAddressChange} />
-                                        </label>
-                                    </div>
-                                    
-                                    <div className="form-row">
-                                        <label>
-                                            <div className ="label-text">What is your organization's phone number?</div>
-                                            <input type="text" id="phone" placeholder="888-888-8888" value={this.state.Phone} onChange={this.handlePhoneChange} />
-                                        </label>
-                                    </div>
-
-                                    <div className="form-row">
-                                        <label>
-                                            <div className ="label-text">Where is your organization's email?</div>
-                                            <input type="text" id="email" placeholder="example@org.com" value={this.state.Email} onChange={this.handleEmailChange} />
-                                        </label>
-                                    </div>
-
-                                    <div className="form-row">
-                                        <label>
-                                            <div className ="label-text">What device(s) is the organization in need of?</div>
-                                            <div className ="device-types" onChange={this.handleNeedChange}>
-                                                <span id="type"><input type="checkbox" value="Phones" name="need" /> Phones</span>
-                                                <span id="type"><input type="checkbox" value="Laptops" name="need" /> Laptops</span>
-                                                <span id="type"><input type="checkbox" value="Tablets" name="need" /> Tablets</span>
-                                            </div>
-                                        </label>
-                                    </div>
-
-                                    <div className="form-row">
-                                        <label>
-                                            <div className ="label-text">Please give a short description of your organization (50 words max)</div>
-                                            <textarea rows="3" cols="80" id="description" placeholder="Description" value={this.state.Description} onChange={this.handleDescriptionChange} />
-                                        </label>
-                                    </div>
-{/* 
-                                    <div className="form-row">
-                                        <label>
-                                            <div className ="label-text">Organization mission statement:</div>
-                                            <input type="text" id="mission" placeholder="Mission statement" value={this.state.Mission} onChange={this.handleMissionChange} />
-                                        </label>
-                                    </div>
-*/}                                 
-                                    <div id="submit-button">
-                                        <button id="submitButton" className="button" onClick={this.handleSubmit} aria-live="assertive" role="submitButton">Submit organization goal</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-
-                    </div>
-                </main>
-            </div>
-        )
-    }
+    setName('')
+    setType('')
+    setAddress('')
+    setPhone('')
+    setEmail('')
+    setNeed('')
+    setGoal('')
+    setDescription('')
 }
 
-export default submitOrg
+
+    return (
+        <div className="form-flex">
+            <form className="form" onSubmit={handleSubmit}>
+                <h1>Submit Organization Goal</h1>
+                
+                <div className = "org-name-type">
+                    <div className = "form-row">
+                        <label>
+                            <div className ="label-text">What is your organization?</div>
+                            <input 
+                                type = "text"
+                                id = "name"
+                                placeholder="Organization name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                        </label>
+                    </div>
+
+                    <div className = "form-row">
+                        <label>
+                            <div className ="label-text">Organization type:</div>
+                            <select id="myList" value={type} onChange={(e) => setType(e.target.value)}>
+                                <option value="0">Select organization type</option>
+                                <option value="1">Medical</option>
+                                <option value="2">Educational</option>
+                                <option value="3">Occupational</option>
+                                <option value="4">Military</option>
+                            </select>
+                        </label>
+                    </div>
+                </div>
+                
+                <div className = "form-row">
+                    <label>
+                        <div className ="label-text">Where is the organization located?</div>
+                        <input 
+                            type = "text"
+                            id = "address"
+                            placeholder="Organization address"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                        />
+                    </label>
+                </div>
+
+                <div className = "form-row">
+                    <label>
+                        <div className ="label-text">What is the organization's phone number?</div>
+                        <input 
+                            type = "text"
+                            id = "phone"
+                            placeholder="888-888-8888"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                    </label>
+                </div>
+
+                <div className = "form-row">
+                    <label>
+                        <div className ="label-text">What is the organization's email?</div>
+                        <input 
+                            type = "text"
+                            id = "email"
+                            placeholder="example@org.com"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </label>
+                </div>
+
+                <div className = "form-row">
+                    <label>
+                        <div className ="label-text">What device(s) is the organization in need of?</div>
+                        <div className ="device-types" onChange={(e) => setNeed(e.target.value)}>
+                            <span id="type"><input type="checkbox" value="Phones" name="need" /> Phones</span>
+                            <span id="type"><input type="checkbox" value="Laptops" name="need" /> Laptops</span>
+                            <span id="type"><input type="checkbox" value="Tablets" name="need" /> Tablets</span>
+                        </div>
+                    </label>
+                </div>
+
+                <div className = "form-row">
+                    <label>
+                        <div className ="label-text">Donation goal:</div>
+                        <input 
+                            type = "text"
+                            id = "goal"
+                            placeholder="100"
+                            value={goal}
+                            onChange={(e) => setGoal(e.target.value)}
+                        />
+                    </label>
+                </div>
+
+                <div className = "form-row">
+                    <label>
+                        <div className ="label-text">Please provide a short description of the organization</div>
+                        <textarea rows="3" cols="80"
+                            id = "description"
+                            placeholder="Description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+                    </label>
+                </div>
+
+                <div id="submit-button">
+                    <button type='submit'>
+                        Submit 
+                    </button>
+                </div>
+
+            </form>
+        </div>
+    )
+}
+
+export default Organization
